2e770eaa41c971442b6b9f1c3a0e85c4fd78be3d
khaleddost <allcanadianrejects@gmail.com>
2024-02-27 08:49:31 -0800

    Branding

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Khaledstudios',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
