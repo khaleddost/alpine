53ee0908f53859bf908e898aa08c8537dff023c7
khaleddost <allcanadianrejects@gmail.com>
2024-02-27 08:56:54 -0800

    branding

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
     title: 'Khaledstudios',
-    description: 'The minimalist blog theme',
+    description: 'The Personal Portfolio of Khaled Dostzada',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -9,7 +9,7 @@ export default defineAppConfig({
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
@@ -18,21 +18,29 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: false,
+
+        // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine',
+
+        text: 'Khaledstudios'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: '',
+      instagram: 'khaleddost',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      youtube: 'khaleddost'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
